f8e94ebbf322ca4b703e024c10b35f67529c5bf4
IT <icode67658@gmail.com>
2022-04-07 18:42:31 +0800

    added 404 error

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
+app.use(cors());
+app.use(bodyParser.json());
+
+
+// routes
 app.get('/', (req, res) => {
-    res.send({hellocl: 'there'});
+    res.send({hello: 'there!'});
 })
 
+app.use((req, res, next) => {
+    let err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => {
+    console.log(`Server is starting on port ${PORT}`);
+});
