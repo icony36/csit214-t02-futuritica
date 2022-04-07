9dbfa6a2ba85fd27600f97866357b3ad165b04df
IT <icode67658@gmail.com>
2022-04-07 18:34:56 +0800

    dependencies added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({wtf: 'there'});
+    res.send({hellocl: 'there'});
 })
 
 const PORT = process.env.PORT || 5000;
