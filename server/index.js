76414b3d347c80f9c18ba65537baf3efadc32771
IT <icode67658@gmail.com>
2022-04-10 00:28:38 +0800

    server config for client

--- server/index.js
+++ server/index.js
@@ -46,6 +46,22 @@ app.use((req, res, next) => {
 
 app.use(errorHandler);
 
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like our main.js file, or main.css file!
+
+    const path = require('path');
+
+    app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    });
+}
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is starting on port ${PORT}`);
