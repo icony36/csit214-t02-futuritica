6d93056be888c9d932ebb0840807fdd3d36c33dc
IT <icode67658@gmail.com>
2022-04-12 21:15:00 +0800

    updated room aval ui

--- client/src/components/RoomItem.js
+++ client/src/components/RoomItem.js
@@ -1,22 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
+import { AVAL } from '../constants';
 
 const MessageItem = props => {
     const { id, availability, timestamp, capacity, price, bookedBy } = props;
 
     const date = dayjs(timestamp);
 
+    const renderAval = () => {
+        switch(availability){
+            case AVAL.booked:
+                return (
+                    <span className='roomitem-text-aval text-danger'>
+                        BOOKED
+                    </span>
+                )
+            case AVAL.public:
+                return (
+                    <span className='roomitem-text-aval text-success'>
+                        AVAILABLE
+                    </span>
+                )
+            case AVAL.private:
+                return (
+                    <span className='roomitem-text-aval text-warning'>
+                        PRIVATE
+                    </span>
+                )
+        }
+    }
+
     return(
         <Link to={`/room/${id}`}>
                 <li className='list-group-item'>
-                <span className='roomitem-date'>
-                    {date.format("DD-MM-YYYY")}              
-                </span>
-                <span className='roomitem-time'>
-                    {date.format("HH:mm")}              
-                </span>
-            </li>
+                    <span className='roomitem-text'>
+                        {date.format("DD-MM-YYYY")}              
+                    </span>
+                    <span className='roomitem-text'>
+                        {date.format("HH:mm")}              
+                    </span>
+                    {renderAval()}
+                </li>
             </Link>
     )   
 }
--- client/src/containers/RoomList.js
+++ client/src/containers/RoomList.js
@@ -2,9 +2,11 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchRooms } from '../store/actions';
 import RoomItem from '../components/RoomItem';
+import { ROLE, AVAL } from '../constants';
 
 const RoomList = () => {
     const rooms = useSelector(state => state.rooms);
+    const auth = useSelector(state =>  state.auth);
     
     const dispatch = useDispatch();
     
@@ -16,24 +18,47 @@ const RoomList = () => {
        fetchData();
     },[]);
     
-    let roomList = rooms.map(r => (
+    const publicRoomList = rooms.filter(r => (r.availability === AVAL.public || r.availability === AVAL.booked)).map(r=> (
         <RoomItem
-            key={r._id}
-            id={r._id}
-            availability={r.availability}
-            timestamp={r.timestamp}
-            capacity={r.capacity}
-            price={r.price}
-            bookedBy={r.bookedBy ? r.bookedBy.username : null}
-        />
+                    key={r._id}
+                    id={r._id}
+                    availability={r.availability}
+                    timestamp={r.timestamp}
+                    capacity={r.capacity}
+                    price={r.price}
+                    bookedBy={r.bookedBy ? r.bookedBy.username : null}                
+                    />
+    ))
+
+    const privateRoomList = rooms.filter(r => (r.availability === AVAL.private)).map(r=> (
+        <RoomItem
+                    key={r._id}
+                    id={r._id}
+                    availability={r.availability}
+                    timestamp={r.timestamp}
+                    capacity={r.capacity}
+                    price={r.price}
+                    bookedBy={r.bookedBy ? r.bookedBy.username : null}                
+                    />
     ))
 
     return(
         <div className='row'>
             <div className='offset-1 col-sm-10'>
+                {auth.user.role === ROLE.staff && (
+                    <h2>Launched Room</h2>
+                )}
                 <ul className='room-list list-group list-group-flush' >
-                    {roomList}
+                    {publicRoomList}
                 </ul>
+               {auth.user.role === ROLE.staff && (
+                <div>
+                   <h2 style={{marginTop: '2rem'}}>Private Room</h2>
+                   <ul className='room-list list-group list-group-flush' >
+                       {privateRoomList}
+                   </ul>
+                </div>
+               )}
             </div>
             
         </div>
