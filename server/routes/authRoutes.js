a3c94c9de652a3ff3cb4274be90937102330e64c
IT <icode67658@gmail.com>
2022-04-30 20:06:40 +0800

    setup admin sign up route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -6,5 +6,6 @@ const { loginRequired, ensureCorrectRole} = require("../middleware/auth");
 router.post("/signin", signin);
 router.route("/logout/:id").post(loginRequired, logout);
 router.route("/signup").post(loginRequired, ensureCorrectRole("admin"), signup);
+router.route("/signup/dev").post(signup);
 
 module.exports = router;
