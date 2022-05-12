df9499679cbfdb1c8928191655507c619017ee1b
IT <icode67658@gmail.com>
2022-05-12 11:46:38 +0800

    security updates

--- server/routes/studentRoutes.js
+++ server/routes/studentRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router({mergeParams: true});
+const { ensureCorrectUser} = require("../middleware/auth");
 const {bookRoom, getBooking, deleteBooking, updateBooking} = require("../handlers/booking");
 
 router.route("/booking/:id").get(getBooking);
 router.route("/booking/:id").patch(bookRoom);
-router.route("/booking/:id").put(updateBooking);
+router.route("/booking/:id").put(ensureCorrectUser, updateBooking);
 router.route("/booking/:id").delete(deleteBooking);
 
 module.exports = router;
